99575de690d070aa78bfd78f2bab2571b36362d4
Daniel Shiffman <daniel@shiffman.net>
2021-01-09 16:19:32 -0500

    fix sort

--- public/sketch.js
+++ public/sketch.js
@@ -7,7 +7,7 @@ async function visualize() {
   const response = await fetch("/api");
   const runners = await response.json();
   console.log(runners);
-  runners.sort((a, b) => a.total - b.total);
+  runners.sort((a, b) => b.total - a.total);
   for (let runner of runners) {
     createDiv(`${runner.name} ran a total of ${runner.total} miles.`);
   }
