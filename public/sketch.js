e4d6829c649ad2e0e10ad327c0b0a5c7dd965e36
jackm9367 <42113446+jackm9367@users.noreply.github.com>
2021-01-19 12:52:09 +1030

    Update code documentation and logging

--- public/sketch.js
+++ public/sketch.js
@@ -4,10 +4,14 @@ function setup() {
 }
 
 async function visualize() {
+  // Get all runners as json
   const response = await fetch("/api");
   const runners = await response.json();
   console.log(runners);
+  // Sort runners from highest total to lowest
   runners.sort((a, b) => b.total - a.total);
+  // Create a new div for each runner, containing their name and
+  // total distance ran
   for (let runner of runners) {
     createDiv(`${runner.name} ran a total of ${runner.total} miles.`);
   }
